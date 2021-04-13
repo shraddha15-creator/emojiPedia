5f383c3b3076af69c6f652748ed07bc295298fb7
Shraddha Muley <66071175+shraddha15-creator@users.noreply.github.com>
2021-04-13 16:36:57 +0530

    initial committ

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./styles.css";
 
-function App() {
+var emojiPedia = {
+
+  "😀": "Grinning Face",
+  "😃": "Grinning Face with Big Eyes",
+  "😄": "Grinning Face with Smiling Eyes",
+  "😁": "Beaming Face with Smiling Eyes",
+  "😆": "Grinning Squinting Face",
+  "😅": "Grinning Face with Sweat",
+  "🤣": "Rolling on the Floor Laughing",
+  "😂": "Face with Tears of Joy",
+  "🙂": "Slightly Smiling Face",
+  "🙃": "Upside-Down Face",
+  "😊": "smiling",
+  "😳": "disbelief",
+  "😔": "sad",
+  "🥡": "takeout box",
+  "❤️": "love",
+  "😑": "annoyance",
+  "😉": "Winking Face",
+"😇": "Smiling Face with Hearts",
+"😍": "Smiling Face with Heart-Eyes",
+"🤩": "Star-Struck",
+"😘": "Face Blowing a Kiss",
+"😗": "Kissing Face",
+"😚": "Kissing Face with Closed Eyes",
+"😙": "Kissing Face with Smiling Eyes"
+};
+
+var emojisWeKnow = Object.keys(emojiPedia);
+
+export default function App() {
+  const [meaning, setMeaning] = useState("");
+
+  function emojiInputHandler(event) {
+
+    var userInput = event.target.value;
+
+    var meaning = emojiPedia[userInput];
+
+    if (meaning === undefined) {
+      meaning = "This emoji is new for us!!";
+    }
+    setMeaning(meaning);
+  }
+
+  function emojiClickHandler(emoji) {
+    var meaning = emojiPedia[emoji];
+    setMeaning(meaning);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>EmojiPedia</h1>
+
+      <input onChange={emojiInputHandler} />
+
+      <h2> {meaning} </h2>
+
+      <h3 style={{color: "#824596"}}> Emojis We Know </h3>
+      {emojisWeKnow.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+            key={emoji}>
+            {emoji}
+          </span>
+        );
+      })}
     </div>
   );
 }
-
-export default App;
