664c9b1ce5bf81edecf6a5d1c4744a08b8860f27
Shraddha Muley <66071175+shraddha15-creator@users.noreply.github.com>
2021-09-15 21:01:50 +0530

    emoji added

--- src/App.js
+++ src/App.js
@@ -35,71 +35,71 @@ var emojiPedia = {
   "🤑" :"Money-Mouth Face",
   "🤗" :"Hugging Face",
   "🤭" :"Face with Hand Over Mouth",
-  "🤫": "Shushing Face"
-"🤔" :"Thinking Face"
-"🤐": "Zipper-Mouth Face"
-"🤨": "Face with Raised Eyebrow"
-"😐": "Neutral Face"
-"😑" :"Expressionless Face"
-"😶" :"Face Without Mouth"
-"😏": "Smirking Face"
-"😒": "Unamused Face"
-"🙄" :"Face with Rolling Eyes"
-"😬" :"Grimacing Face"
-"🤥" :"Lying Face"
-"😌" :"Relieved Face"
-"😔" :"Pensive Face"
-"😪" :"Sleepy Face"
-"🤤": "Drooling Face"
-"😴": "Sleeping Face"
-"😷": "Face with Medical Mask"
-"🤒" "Face with Thermometer"
-"🤕" "Face with Head-Bandage"
-"🤢" "Nauseated Face"
-"🤮" "Face Vomiting"
-"🤧" "Sneezing Face"
-"🥵" "Hot Face"
-"🥶" "Cold Face"
-"🥴" "Woozy Face"
-"😵" "Dizzy Face"
-"🤯" "Exploding Head"
-"🤠" "Cowboy Hat Face"
-"🥳" "Partying Face"
+  "🤫": "Shushing Face",
+"🤔" :"Thinking Face",
+"🤐": "Zipper-Mouth Face",
+"🤨": "Face with Raised Eyebrow",
+"😐": "Neutral Face",
+"😑" :"Expressionless Face",
+"😶" :"Face Without Mouth",
+"😏": "Smirking Face",
+"😒": "Unamused Face",
+"🙄" :"Face with Rolling Eyes",
+"😬" :"Grimacing Face",
+"🤥" :"Lying Face",
+"😌" :"Relieved Face",
+"😔" :"Pensive Face",
+"😪" :"Sleepy Face",
+"🤤": "Drooling Face",
+"😴": "Sleeping Face",
+"😷": "Face with Medical Mask",
+"🤒": "Face with Thermometer",
+"🤕":"Face with Head-Bandage",
+"🤢": "Nauseated Face",
+"🤮": "Face Vomiting",
+"🤧": "Sneezing Face",
+"🥵": "Hot Face",
+"🥶": "Cold Face",
+"🥴": "Woozy Face",
+"😵": "Dizzy Face",
+"🤯": "Exploding Head",
+"🤠": "Cowboy Hat Face",
+"🥳": "Partying Face",
 
-"😎" "Smiling Face with Sunglasses"
-"🤓" "Nerd Face"
-"🧐" "Face with Monocle"
-"😕" "Confused Face"
-"😟" "Worried Face"
-"🙁" "Slightly Frowning Face"
-"☹️" "Frowning Face"
-"😮" "Face with Open Mouth"
-"😯" "Hushed Face"
-"😲" "Astonished Face"
-"😳" "Flushed Face"
-"🥺" Pleading Face
-"😦" Frowning Face with Open Mouth
-"😧" Anguished Face
-"😨" Fearful Face
-"😰" Anxious Face with Sweat
-"😥" Sad but Relieved Face
-"😢" Crying Face
-"😭" Loudly Crying Face
-"😱" Face Screaming in Fear
-"😖" Confounded Face
-"😣" Persevering Face
-"😞" Disappointed Face
-"😓" Downcast Face with Sweat
-"😩" Weary Face
-"😫" Tired Face
-"🥱" Yawning Face
-"😤" Face with Steam From Nose
-"😡" Pouting Face
-"😠" Angry Face
-"🤬" Face with Symbols on Mouth
-"😈" Smiling Face with Horns
-"👿" Angry Face with Horns
-  // 💀 Skull
+"😎": "Smiling Face with Sunglasses",
+"🤓": "Nerd Face",
+"🧐": "Face with Monocle",
+"😕": "Confused Face",
+"😟": "Worried Face",
+"🙁": "Slightly Frowning Face",
+"☹️": "Frowning Face",
+"😮": "Face with Open Mouth",
+"😯": "Hushed Face",
+"😲": "Astonished Face",
+"😳": "Flushed Face",
+"🥺": "Pleading Face",
+"😦": "Frowning Face with Open Mouth",
+"😧": "Anguished Face",
+"😨": "Fearful Face",
+"😰": "Anxious Face with Sweat",
+"😥": "Sad but Relieved Face",
+"😢": "Crying Face",
+"😭": "Loudly Crying Face",
+"😱": "Face Screaming in Fear",
+"😖": "Confounded Face",
+"😣": "Persevering Face",
+"😞": "Disappointed Face",
+"😓": "Downcast Face with Sweat",
+"😩": "Weary Face",
+"😫": "Tired Face",
+"🥱": "Yawning Face",
+"😤": "Face with Steam From Nose",
+"😡": "Pouting Face",
+"😠": "Angry Face",
+"🤬": "Face with Symbols on Mouth",
+"😈": "Smiling Face with Horns",
+"👿": "Angry Face with Horns",
+  // 💀 Skull"
   // ☠️ Skull and Crossbones
   // 💩 Pile of Poo
   // 🤡 Clown Face
